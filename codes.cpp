4693814f4877ff3acd2ecacbe9aa2cf63b51f89b
Nasim Ahmed Bulbul <37321554+bulbul13a@users.noreply.github.com>
2022-08-04 15:54:24 +0600

    Update codes.cpp

--- codes.cpp
+++ codes.cpp
@@ -679,3 +679,56 @@ public:
     
 };
 —---------------------------------###############################----------------------------------------
+//Flip columns
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int n,m,k,a[51][51],ans;
+
+void flip(int i){
+    for(int j=0; j<n; j++){
+        if(a[j][i]==1){
+            a[j][i]=0;
+        }else {
+            a[j][i]=1;
+        }
+    }
+}
+
+void solve(int k){
+    if(k==0){
+        int tempAns=0;
+        for(int i=0; i<n; i++){
+            int f=0;
+            for(int j=0; j<m; j++){
+                if(a[i][j]==0){
+                    f=1;
+                    break;
+                }
+            }
+            if(f==0){
+                tempAns++;
+            }
+        }
+        ans=max(ans,tempAns);
+        return;
+    }
+    for(int i=0; i<m; i++){
+        flip(i);
+        solve(k-1);
+        flip(i);
+    }
+}
+
+int main()
+{
+    cin>>n>>m>>k;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>a[i][j];
+        }
+    }
+    solve(k);
+    cout<<ans;
+    return 0;
+}
