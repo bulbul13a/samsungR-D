b1afcd06e6a84758e91e98901fd054c8c2588649
Nasim Ahmed Bulbul <37321554+bulbul13a@users.noreply.github.com>
2022-08-04 18:02:31 +0600

    Update codes.cpp

--- codes.cpp
+++ codes.cpp
@@ -423,59 +423,51 @@ int main()
 //Research Team
 —---------------------------------###############################----------------------------------------
 //Spaceship//Bomb explosion
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void updateMatrix(int row,char ** matrix){
-    if(row<0){
+
+int arr[20][5],n,ans;
+
+void maxCoins(int row, int col, int temp, int bomb, int effect){
+    if(row==-1){
+        ans=max(ans,temp);
         return;
     }
-    int upLimit=max(0,row-4);
-    for(int i=row;i>=upLimit;i--){
-        for(int j=0;j<=4;j++){
-            if(matrix[i][j]=='2'){
-                matrix[i][j]='0';
+    for(int i=-1; i<=1; i++){
+        if((col+i)<0 || (col+i)>4){
+            continue;
+        }
+        if(arr[row][col+i]==1 || arr[row][col+i]==0){
+            if(bomb==0){
+                maxCoins(row-1,col+i,temp+arr[row][col+i],bomb,effect-1);
+            }else{
+                maxCoins(row-1,col+i,temp+arr[row][col+i],bomb,effect);
+            }
+        }else{
+            if(bomb==0){
+                if(effect>0){
+                    maxCoins(row-1,col+i,temp,bomb,effect-1);
+                }
+            }else{
+                maxCoins(row-1,col+i,temp,0,5);
             }
         }
     }
+    return;
 }
-int findMaxPoints(int row,int col,int bombs,char ** matrix){
-    if(row<=0 || col<0 || col>=5){
-        return 0;
-    }
-    int answer=0;
-    if(row>0 && matrix[row-1][col]!='2'){
-        answer=max(answer,(matrix[row-1][col]=='1'?1:0)+findMaxPoints(row-1,col,bombs,matrix));
-    }
-    if(col>0 && row>0 && matrix[row-1][col-1]!='2'){
-        answer=max(answer,(matrix[row-1][col-1]=='1'?1:0)+findMaxPoints(row-1,col-1,bombs,matrix));
-    }
-    if(col<4 && row>0 && matrix[row-1][col+1]!='2'){
-        answer=max(answer,(matrix[row-1][col+1]=='1'?1:0)+findMaxPoints(row-1,col+1,bombs,matrix));
-    }
 
-    if(answer==0 && bombs>0){
-        updateMatrix(row-1,matrix);
-        answer=findMaxPoints(row,col,bombs-1,matrix);
-    }
-    return answer;
-}
-int main(){
-    int t, row;
-    cin >> t;
-    int tNo = 0;
-    while(t--){
-        cin >> row;
-        char ** matrix=new char*[row + 2];
-        for(int i=0; i<row; i++){
-            matrix[i]=new char[5];
-            for(int j=0;j<5;j++){
-                cin>>matrix[i][j];
-            }
+int main()
+{
+    cin>>n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<5; j++){
+            cin>>arr[i][j];
         }
-        tNo++;
-        cout<< "#" << tNo << " : " << findMaxPoints(row,2,1,matrix) << endl;
     }
+    ans=0;
+    maxCoins(n-1,2,0,1,0);
+    cout<<ans;
     return 0;
 }
 —---------------------------------###############################----------------------------------------
